4df5b8c05845b51fcee69600a6862ea33c544f37
Vasiliy <vasilij.1357@yandex.ru>
2020-11-05 20:33:36 +0300

    Start our test flights project

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="app">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <FiltersBar/>
+    <Flights/>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import FiltersBar from "./components/FiltersBar"
+import Flights from "./components/Flights"
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    FiltersBar,
+    Flights
+  }
+}
+</script>
+
+<style>
+#app {
+  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
+  /*-webkit-font-smoothing: antialiased;*/
+  /*-moz-osx-font-smoothing: grayscale;*/
+  /*text-align: center;*/
+  /*color: #2c3e50;*/
+  /*margin-top: 60px;*/
+}
+</style>
