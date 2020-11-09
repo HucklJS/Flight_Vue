adadffbe76ea366fde0260943397691ba6629cd2
Vasiliy <vasilij.1357@yandex.ru>
2020-11-09 03:58:35 +0300

    Fix bug with no flights on page

--- src/components/Flights.vue
+++ src/components/Flights.vue
@@ -1,40 +1,43 @@
 <template>
   <main>
-    <div
-      class="flight-card"
-      v-for="f in flightsOnPage"
-      :key="f.flightToken"
-    >
-      <div class="flight-header">
-        <div class="logo">{{f.flight.carrier.airlineCode}} logo</div>
-        <div class="price">
-          <strong class="price-value">{{f.flight.price.total.amount}}</strong> стоимость для одного взрослого пассажира
+    <template v-if="flightsOnPage.length">
+      <div
+        class="flight-card"
+        v-for="f in flightsOnPage"
+        :key="f.flightToken"
+      >
+        <div class="flight-header">
+          <div class="logo">{{f.flight.carrier.airlineCode}} logo</div>
+          <div class="price">
+            <strong class="price-value">{{f.flight.price.total.amount}}</strong> стоимость для одного взрослого пассажира
+          </div>
         </div>
+        <Ticket
+          v-for="leg in f.flight.legs"
+          :key="leg.duration"
+
+          :departure-city="leg.segments[0].departureCity.caption"
+          :departure-airport="leg.segments[0].departureAirport.caption"
+          :arrival-city="leg.segments[leg.segments.length - 1].arrivalCity.caption"
+          :arrival-airport="leg.segments[leg.segments.length - 1].arrivalAirport.caption"
+
+          :departure-date="leg.segments[0].departureDate"
+          :arrival-date="leg.segments[leg.segments.length - 1].arrivalDate"
+          :duration="leg.duration"
+
+          :transfers="leg.segments.length - 1"
+
+          :carrier="f.flight.carrier.caption"
+        />
+        <a href="https://www.google.com/" class="order-page">Заказать</a>
       </div>
-      <Ticket
-        v-for="leg in f.flight.legs"
-        :key="leg.duration"
 
-        :departure-city="leg.segments[0].departureCity.caption"
-        :departure-airport="leg.segments[0].departureAirport.caption"
-        :arrival-city="leg.segments[leg.segments.length - 1].arrivalCity.caption"
-        :arrival-airport="leg.segments[leg.segments.length - 1].arrivalAirport.caption"
-
-        :departure-date="leg.segments[0].departureDate"
-        :arrival-date="leg.segments[leg.segments.length - 1].arrivalDate"
-        :duration="leg.duration"
-
-        :transfers="leg.segments.length - 1"
-
-        :carrier="f.flight.carrier.caption"
-      />
-      <a href="https://www.google.com/" class="order-page">Заказать</a>
-    </div>
-
-    <button
-      class="show-more-flights"
-      @click="$emit('increase-number-of-flights-per-page')"
-    >Показать еще</button>
+      <button
+        class="show-more-flights"
+        @click="$emit('increase-number-of-flights-per-page')"
+      >Показать еще</button>
+    </template>
+    <div v-else class="no-flights">По этим фильтрам отображать нечего:(</div>
   </main>
 </template>
 
